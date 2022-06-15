c28ea16f84522a906cdb14655cd8fffc59ecc60e
Keimancer <senjoukeima@gmail.com>
2022-06-15 01:05:57 -0500

    I added a lot of functionalities, still lacking responsivity and some other funct.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const items = [
     {
         id: 1,
         name: 'Hoodie',
-        price: "24.00",
+        price: 24.00,
         image: './assets/images/featured1.png',
         category: 'hoodies',
         quantity: 10
@@ -12,7 +12,7 @@ const items = [
     {
         id: 2,
         name: 'Sweatshirt',
-        price: "14.00",
+        price: 14.00,
         image: './assets/images/featured2.png',
         category: 'sweatshirts',
         quantity: 15
@@ -20,15 +20,15 @@ const items = [
     {
         id: 3,
         name: 'Hoodie',
-        price: "24.00",
+        price: 23.00,
         image: './assets/images/featured3.png',
-        category: 'shirts',
+        category: 'hoodies',
         quantity: 20
     },
     {
         id: 4,
         name: 'Sweatshirt',
-        price: "14.00",
+        price: 13.00,
         image: './assets/images/home.png',
         category: 'sweatshirts',
         quantity: 15
@@ -36,16 +36,26 @@ const items = [
 ];
 
 let nav = document.querySelector("nav");
+let scrollHome = document.querySelector("#home");
+let scrollProds = document.querySelector("#products-scroll");
+let burgerMenu = document.querySelector("#burger-menu");
 let cartIcon = document.querySelector(".cart-icon");
 let cartOpen = document.querySelector("aside.shopping-cart");
 let cartClose = document.getElementById("cart-close");
 let productsContainer = document.querySelector("div.products-father");
+let statesSelector = document.querySelector("div.states");
+let totalPriceSelector = document.querySelector("div.items-count-price");
+let iconCartNumber = document.querySelector("#cart-count");
 let cartTitle = document.querySelector("div.product-list > h2");
-let isCartFT = false;
+let categoryAll = document.querySelector("#category-all");
+let categoryHoodies = document.querySelector("#category-hoodies");
+let categoryShirts = document.querySelector("#category-shirts");
+let categorySweatshirts = document.querySelector("#category-sweatshirts");
+let categorySelected;
 let cart = [];
 
 document.addEventListener("DOMContentLoaded", () =>{
-    showMerch()
+    showMerch(items);
 });
 
 window.addEventListener( "scroll", () =>{
@@ -54,6 +64,14 @@ window.addEventListener( "scroll", () =>{
     }else{
         nav.classList.remove("scroll-change")
     }
+})
+
+scrollHome.addEventListener( "click", () =>{
+    burgerMenu.checked = false;
+})
+
+scrollProds.addEventListener( "click", () =>{
+    burgerMenu.checked = false;
 })
 
 cartIcon.addEventListener( "click", () =>{
@@ -65,11 +83,36 @@ cartClose.addEventListener( "click", () =>{
     cartOpen.classList.remove("show")
 })
 
+categoryAll.addEventListener( "click", () =>{
+    showMerch(items)
+})
 
-function showMerch() {
+categoryHoodies.addEventListener( "click", () =>{
+    let returnFilter = items.filter( (element)=>{
+        return ( element.category == "hoodies" )
+    })
+    showMerch(returnFilter)
+})
+
+categoryShirts.addEventListener( "click", () =>{
+    let returnFilter = items.filter( (element)=>{
+        return ( element.category == "shirts" )
+    })
+    showMerch(returnFilter)
+})
+
+categorySweatshirts.addEventListener( "click", () =>{
+    let returnFilter = items.filter( (element)=>{
+        return ( element.category == "sweatshirts" )
+    })
+    showMerch(returnFilter)
+})
+
+function showMerch(array) {
     let fragmentHTML = ""
 
-    items.forEach( (product) =>{
+
+    array.forEach( (product) =>{
         fragmentHTML += `
         <div class="product-slide">
             <img src=${product.image} alt="">
@@ -77,7 +120,7 @@ function showMerch() {
                 <i class='product-img bx bx-plus'></i>
             </button>
             <div class="product-text">
-                <h2 class="product-price">$${product.price}</h2>
+                <h2 class="product-price">$${product.price.toFixed(2)}</h2>
                 <h5 class="product-stock">| Stock: ${product.quantity}</h5>
             </div>
             <h4 class="product-category">${product.name}</h4>
@@ -94,32 +137,88 @@ function showMerch() {
     addButtonProd.forEach( (button) =>{
         button.addEventListener("click", () =>{
             let id = parseInt( button.getAttribute("data-id") )
-            let product = items.find( item =>{ 
+            let product = array.find( item =>{ 
                 return item.id === id 
             })
-            
-            cart.push( product )
-            console.log((cart))
+            addProduct(product);
         })
     })
 }
 
 function cartFiller(){
     let fragmentHTML = "";
-    let emptyState = document.createElement("div");
-    emptyState.classList.add("empty-state");
     let emptySelector = document.querySelector("div.empty-state");
 
-    if (cart.length == 0 && isCartFT == false){
+    if (cart.length == 0){
         fragmentHTML += `
-            <img src="./assets/images/empty-cart.png" alt="">
-            <h2>Your cart is empty</h2>
-            <h5>You can add items to your cart by clicking on the '+' button on the product page.</h5>
+            <div class="empty-state">
+                <img src="./assets/images/empty-cart.png" alt="">
+                <h2>Your cart is empty</h2>
+                <h5>You can add items to your cart by clicking on the '+' button on the product page.</h5>
+            </div>
         `
-        emptyState.innerHTML = fragmentHTML;
-        cartTitle.insertAdjacentElement("afterend", emptyState);
-        isCartFT = true;
-    }else if (cart.length > 0){
-        emptySelector.parentNode.removeChild(emptySelector);
+        statesSelector.innerHTML = fragmentHTML;
+    }else{
+        addToCart();
     }
 }
+
+function addProduct( product ){
+    let resultFind = cart.find( item => item.id === product.id );
+    if ( resultFind ){
+        let stock = cart[resultFind.index].quantity;
+        let quantitySelected = cart[resultFind.index].quantitySelected;
+        if( stock > quantitySelected ){
+            cart[resultFind.index].quantitySelected++;
+        }else{
+            alert("Easy there, we have a limited stock :)");
+        };
+    }else{
+        product.quantitySelected = 1;
+        product.index = cart.length;
+
+        cart.push(product);
+    }
+
+    addToCart();
+}
+
+function addToCart(){
+    let fragmentHTML = ``;
+    let priceFragmentHTML = ``;
+    let finalQuantity = 0;
+    let finalPrice = 0;
+    cart.forEach( item => {
+        let subtotalCount = (item.quantitySelected * item.price).toFixed(2);
+        fragmentHTML += `
+        <div class="filled-state">
+            <div class="product-in-cart">
+                <img src="${item.image}" alt="">
+                <div class="product-cart-info">
+                    <h2>${item.name}</h2>
+                    <div class="stock-price">
+                        <h3>Stock: ${item.quantity} |</h3>
+                        <h4>$${item.price.toFixed(2)}</h4>
+                    </div>
+                    <h5>Subtotal: $${subtotalCount}</h5>
+                    <div class="units-cart">
+                        <i class='bx bx-minus-circle'></i>
+                        <h6>${item.quantitySelected} units</h6>
+                        <i class='bx bx-plus-circle'></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        finalPrice += item.quantitySelected * item.price;
+        finalQuantity += item.quantitySelected;
+    } )
+    statesSelector.innerHTML = fragmentHTML;
+
+    priceFragmentHTML += `
+        <h3>${finalQuantity} Items</h3>
+        <h3>$${(finalPrice.toFixed(2))}</h3>
+    `
+    iconCartNumber.textContent = finalQuantity;
+    totalPriceSelector.innerHTML = priceFragmentHTML;
+};
